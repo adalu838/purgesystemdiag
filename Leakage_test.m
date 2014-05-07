89ee2be2cf45a44cfd24640dfb5afea631a5da68
Adam <adalu838@student.liu.se>
2014-05-07 16:35:18 +0200

    Algore funkar

--- Leakage_test.m
+++ Leakage_test.m
@@ -6,7 +6,6 @@ function res=Leakage_test( data, biasIdx, diagIdx, pfa)
 %   2) biasIdx  (Samples [start end] for bias estimation)
 %   3) diagIdx  (Samples [start end] for diagnosis sequence)
 %   4) pfa,     (Probability of false alarm)
-%
 % Outputs:
 %   res is a struct variable which includes:
 %   1) T, (The test quantity)
@@ -20,10 +19,24 @@ function res=Leakage_test( data, biasIdx, diagIdx, pfa)
   % 3. TQCalc()
   
   %daigdata = data()
+  y = data.y;
+  k1 = data.k1;
+  Ts = data.Ts;
   
-  T = 0;
-  J = 0;
-  R = 0;
+%   T = 0;
+%   J = 0;
+%   R = 0;
+  % Estimate bias
+  bhat = EstimateBias(y, biasIdx);
+  
+  % Extract data
+  diagdata = y(diagIdx(1):diagIdx(2)) - bhat;
+  
+  % Estimate variance
+  s2 = EstimateVariance(diagdata);
+  
+  % Compute test quantity, residual, and threshold
+  [T, R, J] = TQCalc(diagdata,s2,pfa,k1,Ts);
   
   % Return results
   res.T = T;
@@ -32,21 +45,29 @@ function res=Leakage_test( data, biasIdx, diagIdx, pfa)
 end
 
 
-function bhat = EstimateBias(y)
+function bhat = EstimateBias(y,index)
   % Estimate bias
-  bhat = ;
+  bhat = mean(y(index(1):index(2)));
 end
 
 function s2 = EstimateVariance(y)
   % Estimate variance
-  
-  s2 = ;
+  N = length(y);
+  Y = y(2:end);
+  phi = [y(1:(end-1)) sqrt(-y(1:(end-1))) ones(N-1,1)];
+  theta_hat = phi\Y;
+  r = Y - phi*theta_hat;
+  s2 = cov(r);
 end
 
 function [T, R, J] = TQCalc(y,s2,pfa,k1,Ts)
   % Compute test quantity, residual, and threshold
-  
-  R = ;
-  T = ;
-  J = ;
+  N = length(y);
+  alpha = 1 - Ts*k1;
+  Y = y(2:end) - alpha*y(1:(end-1));
+  phi = ones(N-1,1);
+  theta_hat = phi\Y;
+  R = Y - phi*theta_hat;
+  T = 1/s2*R'*R;
+  J = chi2inv(1-pfa, N-1);
 end
--- lab2_main.m
+++ lab2_main.m
@@ -24,12 +24,12 @@ d = diagnosisSequences.F05;
 
 figure(2);
 subplot(211)
-plot( d.time, d.y);
+plot(d.y);
 title('F05')
 subplot(212)
-plot( d.time, d.D);
+plot(d.D);
 hold on;
-plot( d.time, d.P,'r');
+plot(d.P,'r');
 legend('Diagnosis valve position','Purge valve position');
 hold off;
 
@@ -37,12 +37,12 @@ d = diagnosisSequences.F10;
 
 figure(3);
 subplot(211)
-plot( d.time, d.y);
+plot(d.y);
 title('F10')
 subplot(212)
-plot( d.time, d.D);
+plot(d.D);
 hold on;
-plot( d.time, d.P,'r');
+plot(d.P,'r');
 legend('Diagnosis valve position','Purge valve position');
 hold off;
 
@@ -50,12 +50,12 @@ d = diagnosisSequences.F35;
 
 figure(4);
 subplot(211)
-plot( d.time, d.y);
+plot(d.y);
 title('F35')
 subplot(212)
-plot( d.time, d.D);
+plot(d.D);
 hold on;
-plot( d.time, d.P,'r');
+plot(d.P,'r');
 legend('Diagnosis valve position','Purge valve position');
 hold off;
 
@@ -63,12 +63,12 @@ d = diagnosisSequences.F50;
 
 figure(5);
 subplot(211)
-plot( d.time, d.y);
+plot(d.y);
 title('F50')
 subplot(212)
-plot( d.time, d.D);
+plot(d.D);
 hold on;
-plot( d.time, d.P,'r');
+plot(d.P,'r');
 legend('Diagnosis valve position','Purge valve position');
 hold off;
 
@@ -76,37 +76,37 @@ d = diagnosisSequences.Fxx;
 
 figure(6);
 subplot(211)
-plot( d.time, d.y);
+plot(d.y);
 title('Fxx')
 subplot(212)
-plot( d.time, d.D);
+plot(d.D);
 hold on;
-plot( d.time, d.P,'r');
+plot(d.P,'r');
 legend('Diagnosis valve position','Purge valve position');
 hold off;
 
 %% *** Set index intervals for all datasets
-diagnosisSequences.F00.biasIdx  = []; %(Samples [start end] for bias estimation)
-diagnosisSequences.F00.diagIdx  = []; %(Samples [start end] for diagnosis sequence 1)
-diagnosisSequences.F00.diagIdx2 = []; %(Samples [start end] for diagnosis sequence 2)
+diagnosisSequences.F00.biasIdx  = [953 1011];   %(Samples [start end] for bias estimation)
+diagnosisSequences.F00.diagIdx  = [226 903];    %(Samples [start end] for diagnosis sequence 1)
+diagnosisSequences.F00.diagIdx2 = [1330 2014];  %(Samples [start end] for diagnosis sequence 2)
 
-diagnosisSequences.F10.biasIdx  = [];
-diagnosisSequences.F10.diagIdx  = [];
-diagnosisSequences.F10.diagIdx2 = [];
+diagnosisSequences.F05.biasIdx  = [9200 10000]; %
+diagnosisSequences.F05.diagIdx  = [1753 8646];  %
 
-diagnosisSequences.F35.biasIdx  = [];
-diagnosisSequences.F35.diagIdx  = [];
-diagnosisSequences.F35.diagIdx2 = [];
+diagnosisSequences.F10.biasIdx  = [1 76];       %
+diagnosisSequences.F10.diagIdx  = [476 1021];   %
+diagnosisSequences.F10.diagIdx2 = [1583 2193];  %
 
-diagnosisSequences.F50.biasIdx  = [];
-diagnosisSequences.F50.diagIdx  = [];
-diagnosisSequences.F50.diagIdx2 = [];
+diagnosisSequences.F35.biasIdx  = [1120 1426];  %
+diagnosisSequences.F35.diagIdx  = [891 999];    %
+diagnosisSequences.F35.diagIdx2 = [1737 1830];  %
 
-diagnosisSequences.F05.biasIdx  = [];
-diagnosisSequences.F05.diagIdx  = [];
+diagnosisSequences.F50.biasIdx  = [970 1500];   %
+diagnosisSequences.F50.diagIdx  = [782 820];    %
+diagnosisSequences.F50.diagIdx2 = [1938 1975];  %
 
-diagnosisSequences.Fxx.biasIdx  = [];
-diagnosisSequences.Fxx.diagIdx  = [];
+diagnosisSequences.Fxx.biasIdx  = [9275 9999];  %
+diagnosisSequences.Fxx.diagIdx  = [1717 8697];  %
 
 %% *** Detection results ***
 pfa  = 0.01; % Probability of false alarm
